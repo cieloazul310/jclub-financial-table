5c1bf4c897b419c6cc06fd05a3219c572d29d648
cieloazul310 <cieloazul310@gmail.com>
2026-01-12 14:00:36 +0900

    chore(web): update

--- apps/web/src/app/docs/(post)/sitemap.ts
+++ apps/web/src/app/docs/(post)/sitemap.ts
@@ -2,6 +2,9 @@ import type { MetadataRoute } from "next";
 import { docs } from "@/content";
 import { siteUrl } from "@/data/site-metadata";
 
+export const dynamic = "force-static";
+export const revalidate = false;
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const allDocs = await docs.getAll();
 
