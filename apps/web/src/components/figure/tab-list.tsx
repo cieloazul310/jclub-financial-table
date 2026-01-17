09d00fe1499c6fa428a2dfd06d455753c7012982
cieloazul310 <cieloazul310@gmail.com>
2026-01-17 20:05:32 +0900

    chore(web): update

--- apps/web/src/components/figure/tab-list.tsx
+++ apps/web/src/components/figure/tab-list.tsx
@@ -1,25 +1,31 @@
+"use client";
+
 import { css } from "styled-system/css";
-import { Tabs } from "@/components/ui/tabs";
+import { Tabs, useTabsContext } from "@/components/ui/tabs";
 import { tabOptions } from "@/data/tab";
 
 export function TabList() {
+  const { value } = useTabsContext();
   return (
-    <Tabs.List
-      className={css({
-        position: "sticky",
-        top: "{sizes.header-height}",
-        zIndex: "calc({zIndex.docked} - 1)",
-        bg: "white/85",
-        backdropFilter: "blur(2px)",
-        maxWidth: "90em",
-        mx: "auto",
-      })}
-    >
-      {tabOptions.map(({ id, label }) => (
-        <Tabs.Trigger key={id} py={4} value={id}>
-          {label}
-        </Tabs.Trigger>
-      ))}
-    </Tabs.List>
+    <>
+      <Tabs.List
+        className={css({
+          position: "sticky",
+          top: "{sizes.header-height}",
+          zIndex: "calc({zIndex.docked} - 1)",
+          bg: "white/85",
+          backdropFilter: "blur(2px)",
+          maxWidth: "90em",
+          mx: "auto",
+        })}
+      >
+        {tabOptions.map(({ id, label }) => (
+          <Tabs.Trigger key={id} py={4} value={id}>
+            {label}
+          </Tabs.Trigger>
+        ))}
+      </Tabs.List>
+      <p>currentTab: {value}</p>
+    </>
   );
 }
