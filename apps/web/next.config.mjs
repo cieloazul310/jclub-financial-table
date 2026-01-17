1ec8915bf74e0fc4c9cb3c373ce81bc80146c5b4
cieloazul310 <cieloazul310@gmail.com>
2026-01-17 16:52:55 +0900

    chore(web): update

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -12,7 +12,7 @@ const withMdx = createMDX({
  */
 const nextConfig = {
   output: "export",
-  basePath: "/jclub-financial-next",
+  // basePath: "/jclub-financial-next",
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 };
 
--- apps/web/src/app/(table)/year/[year]/layout.tsx
+++ apps/web/src/app/(table)/year/[year]/layout.tsx
@@ -1,64 +1,18 @@
 import type { PropsWithChildren } from "react";
-import NextLink from "next/link";
-import { HomeIcon } from "lucide-react";
-import { css } from "styled-system/css";
-import { layout } from "styled-system/recipes";
-import { Layout as BaseLayout } from "@/components/layout";
-import { Header } from "@/components/layout/header";
-import { Menu } from "@/components/layout/menu";
-import { PageBottomNav } from "@/components/page-bottom-nav";
-import { SidebarTitle } from "@/components/layout/sidebar-title";
+import { TablePageLayout } from "@/components/layout/table-page";
 
 export default async function Layout({
   children,
   params,
 }: PropsWithChildren<{ params: Promise<{ year: string }> }>) {
   const { year } = await params;
-  const title = `${year}年度の経営情報`;
-  const currentPathname = `/year/${year}`;
-  const headerContent = (
-    <Header
-      title={
-        <span
-          className={css({
-            display: "flex",
-            alignItems: "center",
-            gap: { base: 2, md: 4 },
-          })}
-        >
-          <NextLink href="/" className={css({ mt: ".1em" })}>
-            <HomeIcon />
-          </NextLink>
-          <span>{title}</span>
-        </span>
-      }
-      drawerContent={<Menu currentPathname={currentPathname} />}
-    />
-  );
-  const sidebarContent = (
-    <>
-      <SidebarTitle />
-      <Menu currentPathname={currentPathname} />
-    </>
-  );
 
   return (
-    <BaseLayout
-      classes={layout({
-        breakpoint: "2xl",
-        headerAlways: true,
-        contentWidth: "full",
-        disableContentGutter: true,
-      })}
-      headerContent={headerContent}
-      sidebarContent={sidebarContent}
+    <TablePageLayout
+      title={`${year}年度の経営情報`}
+      currentPathname={`/year/${year}`}
     >
       {children}
-      <PageBottomNav
-        mx="auto"
-        px={{ base: 4, md: 6, lg: 8 }}
-        maxWidth="common-main-width"
-      />
-    </BaseLayout>
+    </TablePageLayout>
   );
 }
