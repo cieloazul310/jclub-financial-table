2ab972902966a015e28467ae867b55fabe6edf9d
cieloazul310 <cieloazul310@gmail.com>
2026-02-08 09:42:37 +0900

    chore: update

---
+++ apps/vercel-api/README.md
@@ -0,0 +1,13 @@
+# Jクラブ経営情報ポータルのREST API (beta)
+
+## エントリポイント
+
+`https://jclub-financial-api.vercel.app/api`
+
+### クラブ
+
+### 年度
+
+### カテゴリ
+
+### プロパティ
---
+++ apps/vercel-api/src/data/site-metadata.ts
@@ -0,0 +1,15 @@
+export const title = "Jクラブ経営情報ポータル API (beta)";
+export const description =
+  "Jリーグが毎年公開している「Jクラブ個別経営情報開示資料」のデータをクラブ別、年度別に取得可能なREST API";
+
+export const lastUpdate = new Date("2026-02-03");
+export const siteUrl = `https://jclub-financial-api.vercel.app`;
+export const repo = `https://github.com/cieloazul310/jclub-financial-table`;
+
+export const siteMetadata = {
+  title,
+  description,
+  siteUrl,
+};
+
+export const postsPerPage = 40;
